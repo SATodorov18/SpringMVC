5708ca0d119a0ec4ce1f950aa5f408d0f40f4106
SATodorov18 <SATodorov18@codingburgas.bg>
2022-11-21 11:53:05 +0200

    done

---
+++ src/main/java/com/example/springmvcapp/SpringMvCappApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvCappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvCappApplication.class, args);
+    }
+
+}
